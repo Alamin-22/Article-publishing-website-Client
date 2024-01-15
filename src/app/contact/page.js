d0b157b801aa25b8059c2a08a671e4dfe91ae0b3
Alamin-22 <mdalaminmollick@gmail.com>
2024-01-15 12:47:44 +0600

    crated basic next.js app and install and tailwindcss and added cotntact page

---
+++ src/app/contact/page.js
@@ -0,0 +1,10 @@
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h2 className='text-green-300 text-5xl flex h-[100vh] justify-center items-center'>This is the container of About us Page</h2>
+        </div>
+    );
+};
+
+export default ContactPage;
