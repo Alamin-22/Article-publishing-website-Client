fab075656d0e4a33781eee33d50c23bb00e67201
shahinsadik <shahinsadik@gmail.com>
2024-01-18 21:47:35 +0600

    map added

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,12 +1,14 @@
 import { FaLocationArrow, FaPhoneSquareAlt, FaMailBulk } from "react-icons/fa";
+
+import LocationMap from "./../../components/LocationMap";
 const ContactPage = () => {
   return (
-    <div className="bg-[#d9d9d9]">
-      <div className="flex    justify-center items-center h-28">
+    <div className="">
+      <div className="flex bg-[#d9d9d9]   justify-center items-center h-28">
         <h2 className="text-3xl font-bold ">Contact Us</h2>
       </div>
 
-      <div className="grid pb-5 grid-cols-1 md:grid-cols-2 justify-center mt-5 ">
+      <div className="grid  pb-5 grid-cols-1 md:grid-cols-2 justify-center items-center mt-5 ">
         <div className="max-w-sm mx-auto">
           <h1 className="text-xl font-semibold">Contact with Us</h1>
           <ul class="space-y-4 mt-3 text-left text-gray-500 dark:text-gray-400">
@@ -80,7 +82,43 @@ const ContactPage = () => {
         </div>
       </div>
 
+      <div className="h-80 overflow-hidden bg-[#d9d9d9] px-20 border-2">
+        <LocationMap />
+      </div>
 
+      <div className="mx-20 mt-20 mb-5 border-2 py-5 border-gray-800 p-2">
+        <div
+          id="newsletter-banner"
+          class="  flex justify-between mx-20 p-4 border-b border-gray-  lg:h-28">
+          <div class="flex items-center  w-full mx-auto sm:w-auto">
+            <form class="flex flex-col gap-5 items-center w-full md:flex-row">
+              <div>
+                <h1 className="text-xl font-bold">
+                  Sign up for our newsletter
+                </h1>{" "}
+                <br />
+                <h4 className="text-sm text-gray-500">
+                  Do you want to get notified when a new Article is <br />
+                  added to scriptly? Sign up for our newsletter
+                </h4>
+              </div>
+              <input
+                type="email"
+                id="newsLetterEmail"
+                placeholder="Enter your email"
+                class="bg-white border border-gray-300 text-gray-900 md:w-64 mb-2 md:mb-0 md:me-4 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                required
+              />
+              <button
+                type="submit"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                Subscribe
+              </button>
+            </form>
+          </div>
+          <div class="flex items-center absolute top-2.5 end-2.5 md:relative md:top-auto md:end-auto"></div>
+        </div>
+      </div>
     </div>
   );
 };
