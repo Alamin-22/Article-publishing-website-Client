100ef287ef60a33d3a9dccabc9a682987066882c
Alamin-22 <mdalaminmollick@gmail.com>
2024-01-18 12:30:59 +0600

    fix project open problem

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,6 +3,7 @@ const page = () => {
     return (
         <div>
             <h1>This is about page.</h1>
+            <h2>Testing</h2>
             <Alert color="info">Alert!</Alert>
         </div>
     );
