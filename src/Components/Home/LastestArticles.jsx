d00203e82c9cae660ece3c06f7b8bd40d06d44b3
pktatari <tawhidalmuhaimin@gmail.com>
2024-01-18 23:03:27 +0600

    minor formatting

--- src/Components/Home/LastestArticles.jsx
+++ src/Components/Home/LastestArticles.jsx
@@ -3,54 +3,68 @@ import LastestCard from './LastestCard';
 import CategoryBox from './CategoryBox';
 
 const LastestArticles = () => {
+    const latestArticlesData = [
+        {
+            title: "Long Title for Latest Article",
+            text: " Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+        {
+            title: "Long Title for Latest Article",
+            text: "Lorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elitLorem ipsum dolor sit amet, consectetur adipiscing elit...",
+            thumbnail: "/marek-levak-GNVxujZ_CxU-unsplash.jpg"
+        },
+    ];
+
+    const categoryData = ["Category A", "Category B", "Category C", "Category D", "Category E"];
+
     return (
         <div className='bg-[#D9D9D9] min-h-screen'>
             <div className='grid grid-cols-4 gap-10 '>
                 <div className='col-span-3'>
-                    <h1 className='font-black text-4xl py-10 pl-5'>Lastest Articles</h1>
+                    <h1 className='font-black text-4xl py-10 pl-5'>Latest Articles</h1>
                     <div className='max-w-7xl pl-10'>
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
-                        <LastestCard
-                            title={"This a Title Sent Through Props"}
-                            text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."}
-                            thumbnail={"/marek-levak-GNVxujZ_CxU-unsplash.jpg"}
-                        />
+                        {latestArticlesData.map((article, index) => (
+                            <LastestCard
+                                key={index}
+                                title={article.title}
+                                text={article.text}
+                                thumbnail={article.thumbnail}
+                            />
+                        ))}
                     </div>
                 </div>
                 <div>
                     <h1 className='font-black text-4xl py-10 pl-5'>Categories</h1>
                     <div className='flex flex-wrap gap-4'>
-                        <CategoryBox categoryName={"categoryName"} />
-                        <CategoryBox categoryName={"Name"} />
-                        <CategoryBox categoryName={"Category"} />
-                        <CategoryBox categoryName={"New Category"} />
-                        <CategoryBox categoryName={"Long category Name"} />
-                        <CategoryBox categoryName={"Sm Name"} />
-                        <CategoryBox categoryName={"categoryName"} />
+                        {categoryData.map((category, index) => (
+                            <CategoryBox key={index} categoryName={category} />
+                        ))}
                     </div>
                 </div>
             </div>
