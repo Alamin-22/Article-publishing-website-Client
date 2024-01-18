3f067abc9d0d900722e5fa5ab88a3f84bb4d5e90
shahinsadik <shahinsadik@gmail.com>
2024-01-18 12:10:01 +0600

    flobite install

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,8 +1,9 @@
-
+import { Alert } from "flowbite-react";
 const page = () => {
     return (
         <div>
             <h1>This is about page.</h1>
+            <Alert color="info">Alert!</Alert>
         </div>
     );
 };
