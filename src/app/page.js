b462a6c39b4cc9c0c4920a7c9bad0efb82d9c1a5
Easin2023 <mdeasinarafat016456@gmail.com>
2024-01-18 15:55:29 +0600

    complete a about page

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
-import React from 'react';
 
 const Home = () => {
   return (
     <div>
       <h2 className='text-5xl text-red-600 flex justify-center items-center h-[100vh]'>
-        This is the HOme Page of Next.js I created my new brach Alamin Brach
+        This is the HOme Page of Next.js 
       </h2>
     </div>
   );
