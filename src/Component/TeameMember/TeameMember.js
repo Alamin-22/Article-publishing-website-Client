762e9a86531fb3269c22be9075437806bb59ed91
Easin2023 <mdeasinarafat016456@gmail.com>
2024-01-19 02:33:32 +0600

    update in about page

---
+++ src/Component/TeameMember/TeameMember.js
@@ -0,0 +1,82 @@
+import Member from "./Member.module.css";
+
+const TeameMember = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <a className={Member.card1} href="#">
+        <figure className="flex justify-center  items-center mt-2">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-ynP26b5_1_N8swK_nVUJXyYgd0Xr9-ryg&usqp=CAU"
+            alt="Shoes"
+            className="rounded-full h-48"
+          />
+        </figure>
+        <div>
+          <h1 className="text-center mt-4 text-2xl">Stephanie</h1>
+          <p className={Member.small}>
+            Card description with lots of great facts and interesting details.
+          </p>
+          <div className={Member.go_corner} href="#">
+            <div className={Member.go_arrow}>→</div>
+          </div>
+        </div>
+      </a>
+      <a className={Member.card1} href="#">
+        <figure className="flex justify-center  items-center mt-2">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-ynP26b5_1_N8swK_nVUJXyYgd0Xr9-ryg&usqp=CAU"
+            alt="Shoes"
+            className="rounded-full h-48"
+          />
+        </figure>
+        <div>
+          <h1 className="text-center mt-4 text-2xl">Stephanie</h1>
+          <p className={Member.small}>
+            Card description with lots of great facts and interesting details.
+          </p>
+          <div className={Member.go_corner} href="#">
+            <div className={Member.go_arrow}>→</div>
+          </div>
+        </div>
+      </a>
+      <a className={Member.card1} href="#">
+        <figure className="flex justify-center  items-center mt-2">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-ynP26b5_1_N8swK_nVUJXyYgd0Xr9-ryg&usqp=CAU"
+            alt="Shoes"
+            className="rounded-full h-48"
+          />
+        </figure>
+        <div>
+          <h1 className="text-center mt-4 text-2xl">Stephanie</h1>
+          <p className={Member.small}>
+            Card description with lots of great facts and interesting details.
+          </p>
+          <div className={Member.go_corner} href="#">
+            <div className={Member.go_arrow}>→</div>
+          </div>
+        </div>
+      </a>
+      <a className={Member.card1} href="#">
+        <figure className="flex justify-center  items-center mt-2">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-ynP26b5_1_N8swK_nVUJXyYgd0Xr9-ryg&usqp=CAU"
+            alt="Shoes"
+            className="rounded-full h-48"
+          />
+        </figure>
+        <div>
+          <h1 className="text-center mt-4 text-2xl">Stephanie</h1>
+          <p className={Member.small}>
+            Card description with lots of great facts and interesting details.
+          </p>
+          <div className={Member.go_corner} href="#">
+            <div className={Member.go_arrow}>→</div>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+export default TeameMember;
--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,86 +1,17 @@
+import AboutUsTitle from "@/Component/AboutUsTitle/AboutUsTitle";
+import TeameMember from "@/Component/TeameMember/TeameMember";
+
 const AboutPage = () => {
   return (
     <div>
-      <div className="bg-slate-300 w-full h-96">
-        <h1 className="flex justify-center items-center h-full text-3xl">
-          About us{" "}
-        </h1>
+      <AboutUsTitle />
+
+      <div className="mx-5 mt-20">
+        <h1 className="text-center text-4xl font-medium my-10">Meet The Team</h1>
+        <TeameMember/>
       </div>
 
-      <div className="flex mt-32 mx-5 gap-16 items-center">
-        <div className="w-96 flex-1 h-80 bg-slate-300 "></div>
-        <div className="flex-1">
-          <h4>about us</h4>
-          <h1 className="text-4xl font-bold mb-3">about title</h1>
-          <p className="text-sm">
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-            fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
-            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut
-            fugit, sed quia consequuntur magni dolores eos qui ratione
-            voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem.
-          </p>
-          <button className="mt-4 px-4 rounded-sm bg-slate-300  ">
-            button
-          </button>
-        </div>
-      </div>
-
-      <div className="mx-5 mt-20 grid grid-cols-3 gap-6">
-        <div className="w-72 rounded-md h-80 shadow-xl bg-slate-300">
-          <figure className="px-24 pt-10 ">
-            {/* <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-full"
-            /> */}
-            <h1 className=" bg-slate-200 w-20 h-20 rounded-full"></h1>
-          </figure>
-          <div className="card-body items-center text-center mx-6">
-            <h2 className="card-title mt-10 mb-3">Hading</h2>
-            <p>
-              If a dog chews shoes whose shoes does he choose? If a dog chews
-              shoes whose shoes does he choose?
-            </p>
-          </div>
-        </div>
-        <div className="w-72 rounded-md h-80 shadow-xl bg-slate-300">
-          <figure className="px-24 pt-10 ">
-            {/* <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-full"
-            /> */}
-            <h1 className=" bg-slate-200 w-20 h-20 rounded-full"></h1>
-          </figure>
-          <div className="card-body items-center text-center mx-6">
-            <h2 className="card-title mt-10 mb-3">Hading</h2>
-            <p>
-              If a dog chews shoes whose shoes does he choose? If a dog chews
-              shoes whose shoes does he choose?
-            </p>
-          </div>
-        </div>
-        <div className="w-72 rounded-md h-80 shadow-xl bg-slate-300">
-          <figure className="px-24 pt-10 ">
-            {/* <img
-              src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
-              alt="Shoes"
-              className="rounded-full"
-            /> */}
-            <h1 className=" bg-slate-200 w-20 h-20 rounded-full"></h1>
-          </figure>
-          <div className="card-body items-center text-center mx-6">
-            <h2 className="card-title mt-10 mb-3">Hading</h2>
-            <p>
-              If a dog chews shoes whose shoes does he choose? If a dog chews
-              shoes whose shoes does he choose?
-            </p>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-slate-300 h-96 my-24">
+      <div className="bg-[#D9D9D9] h-96 my-24">
         <h1 className=" text-center pt-9 text-3xl font-medium">Countdown</h1>
         <div className="grid grid-cols-4 gap-4">
           <div className="bg-slate-200 w-56 h-52 mt-8 rounded-xl mx-5 shadow-xl">
