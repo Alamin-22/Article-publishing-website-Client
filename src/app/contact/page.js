ca0c78c3f9f8d6abcd8a3eb991d4ed7731d7fbd4
Alamin-22 <mdalaminmollick@gmail.com>
2024-01-18 12:21:51 +0600

    fix issue with github

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,5 +1,6 @@
 
 const ContactPage = () => {
+    
     return (
         <div>
             <h2 className='text-green-300 text-5xl flex h-[100vh] justify-center items-center'>This is the container of About us Page</h2>
--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div>
       <h2 className='text-5xl text-red-600 flex justify-center items-center h-[100vh]'>
-        This is the HOme Page of Next.js I created my new brach Alamin Brach
+        This is the HOme Page of Next.js I created my new brach Al-amin Brach
       </h2>
     </div>
   );
