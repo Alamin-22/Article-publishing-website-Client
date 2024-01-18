ba23239f42bda451e4e234fc34bde7f973b3f35c
Easin2023 <mdeasinarafat016456@gmail.com>
2024-01-19 03:02:53 +0600

    new  update in about page

--- src/Component/TeameMember/TeameMember.js
+++ src/Component/TeameMember/TeameMember.js
@@ -3,7 +3,7 @@ import Member from "./Member.module.css";
 const TeameMember = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
-      <a className={Member.card1} href="#">
+      <a className={Member.card1} id="shadow-xl" href="#">
         <figure className="flex justify-center  items-center mt-2">
           <img
             src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS4-ynP26b5_1_N8swK_nVUJXyYgd0Xr9-ryg&usqp=CAU"
