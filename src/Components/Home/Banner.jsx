ee159c87eba678616bcef1318a217044f73130f0
Alamin-22 <mdalaminmollick@gmail.com>
2024-01-19 13:04:22 +0600

    added fixed height into the banner section

--- src/Components/Home/Banner.jsx
+++ src/Components/Home/Banner.jsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 const Banner = () => {
     return (
-        <div className="w-full min-h-screen bg-gray-500 relative">
+        <div className="w-full h-[600px] bg-gray-500 relative">
             <div className="absolute z-50 top-48 left-24">
-                <h1 className="text-gray-800 font-bold text-6xl w-[640px]">Hello there! Welcome to Scriptly</h1>
+                <h1 className="text-white font-bold text-6xl max-w-[640px]">Hello there! Welcome to Scriptly</h1>
                 <p className="text-gray-800 text-xl w-[640px]">The best multipurpose article publishing website</p>
             </div>
-            <div className="opacity-10">
+            <div className="opacity-60">
                 <Image
                     layout="fill"
                     objectFit="cover"
