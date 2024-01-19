161ec0c81441fdb706a7a88ccc76cd3fe8d2da0a
shahinsadik <shahinsadik@gmail.com>
2024-01-19 10:57:05 +0600

    Contact us section Added

--- src/app/page.js
+++ src/app/page.js
@@ -3,11 +3,7 @@ const Home = () => {
   return (
     <div>
       <h2 className='text-5xl text-red-600 flex justify-center items-center h-[100vh]'>
-<<<<<<< HEAD
         This is the HOme Page of Next.js 
-=======
-        This is the HOme Page of Next.js I created my new brach Al-amin Brach
->>>>>>> d98ec68a4778a019d8d20d5131c312ca3873cf3e
       </h2>
     </div>
   );
