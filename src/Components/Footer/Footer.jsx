40fef075991513da1f2bded8d1a7ff1b345f5188
pktatari <tawhidalmuhaimin@gmail.com>
2024-01-22 17:38:21 +0600

    responsive done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import { BsDribbble, BsFacebook, BsGithub, BsInstagram, BsTwitter } from 'react-
 
 function FooterCom() {
     return (
-        <Footer container className='bg-[#ebe3eb]'>
+        <Footer container className='bg-[#D9D9D9]'>
             <div className="w-full ">
                 <div className="grid w-full justify-between sm:flex sm:justify-between md:flex md:grid-cols-1">
                     <div>
