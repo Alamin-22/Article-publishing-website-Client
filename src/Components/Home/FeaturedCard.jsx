a349aa0f5707165e237df88f94332d2f1212db85
pktatari <tawhidalmuhaimin@gmail.com>
2024-01-24 20:10:02 +0600

    updated navbar, footer, banner, lastest section

--- src/Components/Home/FeaturedCard.jsx
+++ src/Components/Home/FeaturedCard.jsx
@@ -4,7 +4,7 @@ const FeaturedCard = ({ idx, title, text }) => {
     return (
         <div className='flex items-center gap-5'>
             <div>
-                <h1 className='text-white font-black text-5xl'>{idx}</h1>
+                <h1 className='text-white font-black text-5xl drop-shadow-[0_1.2px_1.2px_rgba(0,0,0,1)]'>{idx}</h1>
             </div>
             <div>
                 <h1 className='text-gray-800 font-black text-2xl'>{title}</h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,7 @@
 import { Avatar, Dropdown, Navbar } from 'flowbite-react';
 import { Button } from 'flowbite-react';
 import { useRouter } from 'next/router';
+import Logo from './Miscellaneous/Logo';
 
 function NavbarComponent() {
 
@@ -12,9 +13,7 @@ function NavbarComponent() {
     return (
         <Navbar fluid rounded className='shadow-xl bg-[#D9D9D9] '>
             <Navbar.Brand >
-                <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                    <span className='text-[#cd1cd2] text-2xl' >S</span>cr<span className='text-[#da4681]'>ip</span>t<span className='text-[#d24bff]'>ly</span>
-                </span>
+                <Logo className={"text-2xl"} />
             </Navbar.Brand>
             <div className="flex md:order-2 ">
                 {
@@ -40,7 +39,7 @@ function NavbarComponent() {
                         </>
                         :
                         <>
-                            <Button gradientDuoTone="purpleToPink">Get Start</Button>
+                            <Button color='dark' pill>Get Start</Button>
                         </>
                 }
 
@@ -51,8 +50,8 @@ function NavbarComponent() {
                     Home
                 </Navbar.Link>
                 <Navbar.Link href="/about" >About Us</Navbar.Link>
+                <Navbar.Link href="/write">Write</Navbar.Link>
                 <Navbar.Link href="#">Community</Navbar.Link>
-                <Navbar.Link href="#">Support</Navbar.Link>
                 <Navbar.Link href="/contact">Contact</Navbar.Link>
             </Navbar.Collapse>
         </Navbar>
