4924cce3e7c1a0359ca4258a4038cbbe5b8035ca
shahinsadik <shahinsadik@gmail.com>
2024-02-12 15:58:34 +0600

    termsAndCondition added

---
+++ src/app/dashboardlayout/my-articles/MyAllArticle.jsx
@@ -0,0 +1,10 @@
+const MyAllArticle = () => {
+    
+    return (
+        <div>
+            <h2>This is my article page coming from fashboard</h2>
+        </div>
+    );
+};
+
+export default MyAllArticle;
