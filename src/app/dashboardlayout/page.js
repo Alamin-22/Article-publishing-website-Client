409eda656f540292b667c8c10acf86d81dfa3244
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-16 00:40:33 +0600

    fix responsive issue for all the charts

--- src/app/dashboardlayout/page.js
+++ src/app/dashboardlayout/page.js
@@ -1,14 +1,13 @@
 import BarChart from '@/components/DashboardComponents/BarChart';
 import Chart from '@/components/DashboardComponents/Chart';
 import PieChart from '@/components/DashboardComponents/PieChart';
-import Statics from '@/components/DashboardComponents/PieChart';
 import React from 'react';
 
 const dashboard = () => {
     return (
         <div className=' bg-slate-50 '>
             <Chart></Chart>
-            <div className='flex flex-col lg:flex-row'>
+            <div className='flex flex-col lg:flex-row items-center'>
                 <PieChart></PieChart>
                 <BarChart></BarChart>
             </div>
