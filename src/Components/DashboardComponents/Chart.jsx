6385628330bec13c810f04a7fb6223d90b02f97f
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-15 12:20:21 +0600

    added apex chart to the dashboard

--- src/Components/DashboardComponents/Chart.jsx
+++ src/Components/DashboardComponents/Chart.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+"use client"
+import ReactApexChart from 'react-apexcharts'
+
+import React, { useState } from 'react';
 
 const Chart = () => {
+    const [state, setState] = useState({
+
+        series: [{
+            name: 'series1',
+            data: [31, 40, 28, 51, 42, 109, 100]
+        }, {
+            name: 'series2',
+            data: [11, 32, 45, 32, 34, 52, 41]
+        }],
+        options: {
+            chart: {
+                height: 350,
+                type: 'area'
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                curve: 'smooth'
+            },
+            xaxis: {
+                type: 'datetime',
+                categories: ["2018-09-19T00:00:00.000Z", "2018-09-19T01:30:00.000Z", "2018-09-19T02:30:00.000Z", "2018-09-19T03:30:00.000Z", "2018-09-19T04:30:00.000Z", "2018-09-19T05:30:00.000Z", "2018-09-19T06:30:00.000Z"]
+            },
+            tooltip: {
+                x: {
+                    format: 'dd/MM/yy HH:mm'
+                },
+            },
+        },
+
+
+
+    })
+
+
+
+
     return (
         <div>
-            
+            <div className=' xl:w-[350px] md:w-[500px] lg:w-[1000px]  mx-auto'>
+                <ReactApexChart
+                    options={state.options}
+                    series={state.series}
+                    type="area" height={350} className={"w-full"} />
+            </div>
         </div>
     );
 };
 
 export default Chart;
+
+
--- src/app/dashboardlayout/page.js
+++ src/app/dashboardlayout/page.js
@@ -1,3 +1,4 @@
+import Chart from '@/components/DashboardComponents/Chart';
 import Statics from '@/components/DashboardComponents/Statics';
 import React from 'react';
 
@@ -5,8 +6,8 @@ const dashboard = () => {
     return (
         <div className=' h-screen'>
             <h2 className='text-5xl text-white' >this is dashboard</h2>
+            <Chart></Chart>
             <Statics></Statics>
-            
         </div>
     );
 };
