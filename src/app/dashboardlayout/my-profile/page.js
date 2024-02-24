2456e356e8330c0482f4f7f941c12a986b1ee2fa
Alamin-22 <mdalaminmollick@gmail.com>
2024-02-24 12:24:15 +0600

    fix layout into my profile page

--- src/app/dashboardlayout/my-profile/page.js
+++ src/app/dashboardlayout/my-profile/page.js
@@ -9,6 +9,8 @@ import axiosInstance from "@/api";
 import useAuth from "@/Hooks/useAuth";
 import { FileInput, Label } from 'flowbite-react';
 import { FaRegEdit } from "react-icons/fa";
+import { Button } from 'flowbite-react';
+import { HiOutlineArrowRight, HiShoppingCart } from 'react-icons/hi';
 
 const MyProfile = () => {
   const [editCoverPic, setEditCoverPic] = useState(false);
@@ -50,46 +52,26 @@ const MyProfile = () => {
       {/* user profile with banner and title */}
       <figure className="relative">
         <FaRegEdit onClick={handleEditCover} className='absolute right-5 top-5 z-50 text-blue-500 text-xl hover:cursor-pointer hover:text-blue-700' />
-        <div className="w-full h-[350px] bg-black relative rounded-xl">
-          <div className="absolute z-40 h-full w-full flex items-center justify-center text-center flex-col ">
-            <img
-              src={ProfileInfo?.UserPhoto}
-              className="w-36 h-36 mt-5 rounded-full"
-              alt="user Image"
-            />
-            <div className="my-2">
-              <div>
-                <h2 className="text-gray-100 font-black text-3xl lg:text-3xl max-w-80 lg:max-w-[640px]">
-                  {ProfileInfo?.displayName}
-                </h2>
-                <p className="text-zinc-200 font-semibold text-xl max-w-[640px] ">
-                  Full Stack Developer
-                </p>
-              </div>
-              <div className="flex justify-evenly mt-5 text-gray-100 text-3xl gap-5">
-                <div>
-                  <p className="font-semibold ">
-                    {" "}
-                    <CountUp end={25620} duration={8} />{" "}
-                  </p>
-                  <p className="text-zinc-300 text-lg">Followers </p>
-                </div>
-                <div>
-                  <p className="font-semibold ">
-                    <CountUp end={61526} duration={8} />
-                  </p>
-                  <p className="text-zinc-300 text-lg">Following </p>
-                </div>
-              </div>
-            </div>
+        {
+          editCoverPic &&
+          <div className="flex flex-wrap gap-2 absolute left-5 top-5 z-40">
+            <Button  onClick={handleEditCover}>
+              X
+            </Button>
+            <Button>
+              Save Changes
+            </Button>
           </div>
+        }
+        <div className="w-full h-[350px] bg-black relative rounded-xl">
+
           {
             editCoverPic ?
               <>
                 <div className="flex w-full items-center justify-center z-50">
                   <Label
                     htmlFor="dropzone-file"
-                    className="dark:hover:bg-bray-800 flex h-80 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                    className="dark:hover:bg-bray-800 flex h-[350px] w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600"
                   >
                     <div className="flex flex-col items-center justify-center pb-6 pt-5">
                       <svg
@@ -118,6 +100,38 @@ const MyProfile = () => {
               </>
               :
               <>
+                <div className="absolute z-10 h-full w-full flex items-center justify-center text-center flex-col ">
+                  <img
+                    src={ProfileInfo?.UserPhoto}
+                    className="w-36 h-36 mt-5 rounded-full"
+                    alt="user Image"
+                  />
+                  <div className="my-2">
+                    <div>
+                      <h2 className="text-gray-100 font-black text-3xl lg:text-3xl max-w-80 lg:max-w-[640px]">
+                        {ProfileInfo?.displayName}
+                      </h2>
+                      <p className="text-zinc-200 font-semibold text-xl max-w-[640px] ">
+                        Full Stack Developer
+                      </p>
+                    </div>
+                    <div className="flex justify-evenly mt-5 text-gray-100 text-3xl gap-5">
+                      <div>
+                        <p className="font-semibold ">
+                          {" "}
+                          <CountUp end={25620} duration={8} />{" "}
+                        </p>
+                        <p className="text-zinc-300 text-lg">Followers </p>
+                      </div>
+                      <div>
+                        <p className="font-semibold ">
+                          <CountUp end={61526} duration={8} />
+                        </p>
+                        <p className="text-zinc-300 text-lg">Following </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
                 <div className="opacity-50 ">
                   <Image
                     layout="fill"
